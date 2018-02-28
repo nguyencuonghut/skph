84228ca2bdf4dcafbff2b1dc69a5198ae98f21fd
Unknown <nguyencuonghut55@gmail.com>
2018-02-28 13:11:10 +0700

    Add button for user to edit his profile.

--- resources/views/partials/userheader.blade.php
+++ resources/views/partials/userheader.blade.php
@@ -1,8 +1,7 @@
 <div class="col-lg-6">
 
     <div class="profilepic"><img class="profilepicsize" src="../{{ $contact->avatar }}" /></div>
-    <h1>{{ $contact->nameAndDepartment }} </h1>
-
+    <h1>{{ $contact->name }} </h1>
     <!--MAIL-->
     <p><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
         <a href="mailto:{{ $contact->email }}">{{ $contact->email }}</a></p>
@@ -15,6 +14,8 @@
         <a href="tel:{{ $contact->personal_number }}">{{ $contact->personal_number }}</a></p>
 
     <!--Address-->
-    <p><span class="glyphicon glyphicon-home" aria-hidden="true"></span>
-        {{ $contact->address }}  </p>
+    @if(\Auth::id() == $contact->id)
+    <p><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+        <a href="{{ route("users.edit", $contact->id) }}">Sửa</a></p>
+    @endif
 </div>
