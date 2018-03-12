c8d61cca7ea1ac87bcf60bb18fdc0b8adb89809f
Unknown <nguyencuonghut55@gmail.com>
2018-03-12 14:47:34 +0700

    1 22 Change role of Ta Van Toai to admin Translate some english into vietnamese Disable harcode Hide some buttons according to their state. Fix the bug for reason statistic of each department. Update the status of each ticket and show statistic based on the status and reason for each department 1 hour ago Add subject to troubleshoot and prevention notification email 6 days ago Add one more action Add table for troubleshoot/prevention actions statistic. And fix the bug for approve the root cause Add table for troubleshoot/prevention actions statistic. And fix the bug for approve the root cause Add function to display Troubleshootable/Preventionable actions rate Display value of dataset on reason type chart. a week ago Add function to update reason type and display chart of tickets according to each reason type Add the pie chart for tickets according to each department. Add department to description Add user name to notification email Fix the bug that allow user to create new prevention actions Add function to determine a prevention action is done on time or not. And fix the bug of change the is_on_time. Add function to determine a troubleshoot action is done on time or not Cập nhật flash message Add subject to notification email Display the progress bar of troubleshoot actions and prevention actions Add dev name to email footer Add bold to effectiveness on Action Log 23 file changes in working directory View changes commit:b94a06 Change role of Ta Van Toai to admin

--- database/seeds/UserRoleTableSeeder.php
+++ database/seeds/UserRoleTableSeeder.php
@@ -19,7 +19,7 @@ class UserRoleTableSeeder extends Seeder
         $newrole->save();
 
         $newrole = new RoleUser;
-        $newrole->role_id = '2';
+        $newrole->role_id = '1';
         $newrole->user_id = '2';
         $newrole->timestamps = false;
         $newrole->save();
