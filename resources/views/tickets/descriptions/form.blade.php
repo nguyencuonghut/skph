e1b81fa5a4de2fecbd5d7649d5448b35972eaadb
Unknown <nguyencuonghut55@gmail.com>
2018-03-22 09:58:58 +0700

    Change the alignment when create new tick

--- resources/views/tickets/descriptions/form.blade.php
+++ resources/views/tickets/descriptions/form.blade.php
@@ -4,7 +4,7 @@
     </div>
 
     <div class="form-inline">
-        <div class="form-group col-sm-6 removeleft ">
+        <div class="form-group col-sm-6 removeleft">
             {!! Form::label('issue_date', __('Ngày phát hành'), ['class' => 'control-label']) !!}
             {!! Form::date('issue_date', \Carbon\Carbon::now(), ['class' => 'form-control']) !!}
         </div>
@@ -14,67 +14,79 @@
         </div>
     </div>
 
-    <div class="form-group">
-        {!! Form::label('area_id', __('Phát hành tới') , ['class' => 'control-label']) !!}
-        {!! Form::select('area_id', $areas, null, ['placeholder' => '', 'id'=>'area_id', 'name'=>'area_id','class'=>'form-control', 'style' => 'width:100%']) !!}
+    <div class="form-inline">
+        <div class="form-group col-sm-4 removeleft ">
+            {!! Form::label('area_id', __('Phát hành tới') , ['class' => 'control-label']) !!}
+            {!! Form::select('area_id', $areas, null, ['placeholder' => '', 'id'=>'area_id', 'name'=>'area_id','class'=>'form-control']) !!}
+
+        </div>
+        <div class="form-group col-sm-4 removeleft">
+            {!! Form::label('source_id', __('Nguồn gốc') , ['class' => 'control-label']) !!}
+            {!! Form::select('source_id', $sources, null, ['placeholder' => '', 'id'=>'source_id', 'name'=>'source_id','class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group col-sm-4 removeleft removeright">
+            {!! Form::label('action_id', __('Hành động') , ['class' => 'control-label']) !!}
+            {!! Form::select('action_id', $actions, null, ['placeholder' => '', 'id'=>'action_id', 'name'=>'action_id','class'=>'form-control', 'style' => 'width:100%']) !!}
+        </div>
 
     </div>
 
-    <div class="form-group">
-        {!! Form::label('source_id', __('Nguồn gốc') , ['class' => 'control-label']) !!}
-        {!! Form::select('source_id', $sources, null, ['placeholder' => '', 'id'=>'source_id', 'name'=>'source_id','class'=>'form-control', 'style' => 'width:100%']) !!}
-
+    <div class="form-inline">
+        <div class="form-group col-sm-6 removeleft">
+            {!! Form::label('what', __('Cái gì đã xảy ra ?'), ['class' => 'control-label']) !!}
+            {!! Form::text('what', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group col-sm-6 removeleft removeright">
+            {!! Form::label('what', __('Cái gì đã xảy ra ?'), ['class' => 'control-label']) !!}
+            {!! Form::text('what', null, ['class' => 'form-control']) !!}
+        </div>
     </div>
 
-    <div class="form-group">
-        {!! Form::label('action_id', __('Hành động') , ['class' => 'control-label']) !!}
-        {!! Form::select('action_id', $actions, null, ['placeholder' => '', 'id'=>'action_id', 'name'=>'action_id','class'=>'form-control', 'style' => 'width:100%']) !!}
+    <div class="form-inline">
+        <div class="form-group col-sm-6 removeleft">
+            {!! Form::label('why', __('Tại sao đây là một vấn đề ?'), ['class' => 'control-label']) !!}
+            {!! Form::text('why', null, ['class' => 'form-control']) !!}
+        </div>
 
+        <div class="form-group col-sm-6 removeleft removeright">
+            {!! Form::label('when', __('Nó xảy ra khi nào ?'), ['class' => 'control-label']) !!}
+            {!! Form::date('when', \Carbon\Carbon::now(), ['class' => 'form-control']) !!}
+        </div>
     </div>
 
-    <div class="form-group">
-        {!! Form::label('what', __('Cái gì đã xảy ra ?'), ['class' => 'control-label']) !!}
-        {!! Form::text('what', null, ['class' => 'form-control']) !!}
+    <div class="form-inline">
+        <div class="form-group col-sm-4 removeleft">
+            {!! Form::label('who', __('Ai phát hiện ra ?'), ['class' => 'control-label']) !!}
+            {!! Form::text('who', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group col-sm-4 removeleft">
+            {!! Form::label('where', __('Phát hiện ra ở đâu ?'), ['class' => 'control-label']) !!}
+            {!! Form::text('where', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group col-sm-4 removeleft removeright">
+            {!! Form::label('how_1', __('Bằng cách nào ?'), ['class' => 'control-label']) !!}
+            {!! Form::text('how_1', null, ['class' => 'form-control']) !!}
+        </div>
     </div>
 
-    <div class="form-group">
-        {!! Form::label('why', __('Tại sao đây là một vấn đề ?'), ['class' => 'control-label']) !!}
-        {!! Form::text('why', null, ['class' => 'form-control']) !!}
+    <div class="form-inline">
+        <div class="form-group col-sm-4 removeleft">
+            {!! Form::label('how_2', __('Có bao nhiêu sự không phù hợp'), ['class' => 'control-label']) !!}
+            {!! Form::text('how_2', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group col-sm-4 removeleft">
+            {{ Form::label('image', __('Ảnh'), ['class' => 'control-label']) }}
+            {!! Form::file('image',  null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group  col-sm-4 removeleft removeright">
+            {!! Form::label('leader_id', __('Trưởng bộ phận'), ['class' => 'control-label']) !!}
+            {!! Form::select('leader_id', $users, null, ['placeholder' => '', 'id'=>'leader_id', 'name'=>'leader_id','class'=>'form-control', 'style' => 'width:100%']) !!}
+        </div>
     </div>
 
-    <div class="form-group">
-        {!! Form::label('when', __('Nó xảy ra khi nào ?'), ['class' => 'control-label']) !!}
-        {!! Form::date('when', \Carbon\Carbon::now(), ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('who', __('Ai phát hiện ra ?'), ['class' => 'control-label']) !!}
-        {!! Form::text('who', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('where', __('Phát hiện ra ở đâu ?'), ['class' => 'control-label']) !!}
-        {!! Form::text('where', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('how_1', __('Bằng cách nào ?'), ['class' => 'control-label']) !!}
-        {!! Form::text('how_1', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('how_2', __('Có bao nhiêu sự không phù hợp'), ['class' => 'control-label']) !!}
-        {!! Form::text('how_2', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {{ Form::label('image', __('Ảnh'), ['class' => 'control-label']) }}
-        {!! Form::file('image',  null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group form-inline">
-        {!! Form::label('leader_id', __('Trưởng bộ phận'), ['class' => 'control-label']) !!}
-        {!! Form::select('leader_id', $users, null, ['placeholder' => '', 'id'=>'leader_id', 'name'=>'leader_id','class'=>'form-control', 'style' => 'width:100%']) !!}
-    </div>
-
-    {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary']) !!}
+    {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary', 'style' => 'width:100%']) !!}
